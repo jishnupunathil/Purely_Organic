201e1e8fd8aa06c62106fae881ec583cf6b8c637
Jishnu <jishnupunathil000@gmail.com>
2023-03-28 13:05:47 +0530

    ui modified/blog/$

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Purely Organic' });
 });
 
+router.get('/login',(req,res)=>{
+  res.render('user/userLogin')
+})
+
 module.exports = router;
