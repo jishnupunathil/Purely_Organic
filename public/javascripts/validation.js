bb5c83fd456b0e683c91689088f9a0dc0f61638f
Jishnu <jishnupunathil000@gmail.com>
2023-03-30 00:37:08 +0530

    validation added for log/sign

--- public/javascripts/validation.js
+++ public/javascripts/validation.js
@@ -1,109 +1,83 @@
-$(document).ready(function () {
-    jQuery.validator.addMethod(
-      "lettersonly",
-      function (value, element) {
-        return this.optional(element) || /^[a-z,A-Z ]+$/.test(value);
-      },
-      "Letters only please"
-    );
-    jQuery.validator.addMethod(
-      "minlength5",
-      function (value, element) {
-        return this.optional(element) || (value.trim().length >= 5);
-      },
-      "Minimum 5 characters without space"
-    );
-    $("#login-form").validate({
-      rules: {
-        email: {
-          required: true,
-          email: true,
-        },
-        password: {
-          required: true,
-          minlength: 5,
-          maxlength: 15,
-        },
-      },
-      messages: {
-        email: {
-          email: "Please enter a valid Email id",
-        },
-        password: {
-          minlength: "too Short!",
-          maxlength: "too large",
-        },
-      },
-    });
-   
-  });
-
-  
-//signup
+jQuery.validator.addMethod(
+  "lettersonly",
+  function (value, element) {
+    return this.optional(element) || /^[a-z,A-Z ]+$/.test(value);
+  },
+  "Letters only please"
+);
+jQuery.validator.addMethod(
+  "minlength5",
+  function (value, element) {
+    return this.optional(element) || (value.trim().length >= 5);
+  },
+  "Minimum 5 characters without space"
+);
 
 $(document).ready(function () {
-    jQuery.validator.addMethod(
-      "lettersonly",
-      function (value, element) {
-        return this.optional(element) || /^[a-z,A-Z ]+$/.test(value);
+  $("#login").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true,
       },
-      "Letters only please"
-    );
-    jQuery.validator.addMethod(
-      "minlength5",
-      function (value, element) {
-        return this.optional(element) || (value.trim().length >= 5);
+      password: {
+        required: true,
+        minlength: 5,
+        maxlength: 15,
       },
-      "Minimum 5 characters without space"
-    );
-    $("#register-form").validate({
-      rules: {
-        user: {
-          required: true,
-          minlength:5
-        },
-        email: {
-          required: true,
-          email: true,
-        },
-        // phone: {
-        //   required: true,
-        //   number: true,
-        //   minlength: 10,
-        //   maxlength: 10,
-        // },
-        password: {
-          required: true,
-          minlength: 5,
-          maxlength: 15,
-        },
-        confirmPassword: {
-          required: true,
-          equalTo: "#form3Example4cg",
-          minlength5: true,
-          minlength: 5,
-          maxlength: 15,
-        },
+    },
+    messages: {
+      email: {
+        email: "Please enter a valid Email id",
       },
-      messages: {
-        user: {
-          minlength: "Please Enter Your Full Name",
-        },
-        email: {
-          email: "Please enter a valid Email id",
-        },
-        password: {
-          minlength: "Please enter a password more than 5 characters",
-          maxlength: "Please enter a password less than 15 characters",
-        },
-        phone: "Enter valid phone number ",
-        confirmPassword: {
-          equalTo:'password does not match'
-        }
+      password: {
+        minlength: "too Short!",
+        maxlength: "too large",
       },
-    });
-   
+    },
   });
 
+//   $("#signup").validate({
+//     rules: {
+//       user: {
+//         required: true,
+//         minlength: 5,
+//         lettersonly: true,
+//       },
+//       email: {
+//         required: true,
+//         email: true,
+//       },
+//       password: {
+//         required: true,
+//         minlength: 5,
+//         maxlength: 15,
+//       },
+//       confirmPassword: {
+//         required: true,
+//         equalTo: "#form3Example4cg",
+//         minlength5: true,
+//         minlength: 5,
+//         maxlength: 15,
+//       },
+//     },
+//     messages: {
+//       user: {
+//         minlength: "Please Enter Your Full Name",
+//         lettersonly: "Letters only please",
+//       },
+//       email: {
+//         email: "Please enter a valid Email id",
+//       },
+//       password: {
+//         minlength: "Please enter a password more than 5 characters",
+//         maxlength: "Please enter a password less than 15 characters",
+//       },
+//       confirmPassword: {
+//         equalTo:'password does not match',
+//       }
+//     },
+//   });
+});
 
   
---
+++ public/js/validation.js
@@ -0,0 +1,99 @@
+jQuery.validator.addMethod(
+  "lettersonly",
+  function (value, element) {
+    return this.optional(element) || /^[a-z,A-Z ]+$/.test(value);
+  },
+  "Letters only please"
+);
+jQuery.validator.addMethod(
+  "minlength5",
+  function (value, element) {
+    return this.optional(element) || (value.trim().length >= 5);
+  },
+  "Minimum 5 characters without space"
+);
+
+$(document).ready(function () {
+  $("#login").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true,
+      },
+      password: {
+        required: true,
+        minlength: 5,
+        maxlength: 15,
+      },
+    },
+    messages: {
+      email: {
+        email: "Please enter a valid Email id",
+      },
+      password: {
+        minlength: "too Short!",
+        maxlength: "too large",
+      },
+    },
+  });
+
+  $("#signup").validate({
+    rules: {
+      firstname: {
+        required: true,
+        minlength: 5,
+        lettersonly: true,
+      },
+      lastname: {
+        required: true,
+        minlength: 1,
+        lettersonly: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      phoneNumber: {
+        required: true,
+        number: true,
+        minlength: 10,
+        maxlength: 10,
+      },
+      password: {
+        required: true,
+        minlength: 5,
+        maxlength: 15,
+      },
+      confirmPassword: {
+        required: true,
+        equalTo: "#password",
+        minlength5: true,
+        minlength: 5,
+        maxlength: 15,
+      },
+    },
+    messages: {
+      firstname: {
+        minlength: "Please Enter Your first Name",
+        lettersonly: "Letters only please",
+      },
+      lastname: {
+        minlength: "Please Enter Your last Name",
+        lettersonly: "Letters only please",
+      },
+      email: {
+        email: "Please enter a valid Email id",
+      },
+      phoneNumber:"Enter valid phone number ",
+      password: {
+        minlength: "too Short!",
+        maxlength: "too large",
+      },
+      confirmPassword: {
+        equalTo:'password does not match',
+      }
+    },
+  });
+});
+
+  
